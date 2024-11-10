42213e5d7af0775efb21275c8f0448bd6584c213
Stephen Lincoln Strange <stephenlstrange2@gmail.com>
2024-11-10 09:48:41 -0800

    adding libso for wiring x

--- duo-projects/zig/blink/blink.zig
+++ duo-projects/zig/blink/blink.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
 const wx = @cImport({
-    "wiringx.h"
-    });
+    @cInclude("wiringx.h");
+});
 
 pub fn main() !void {
     var board = "duo".*;
     const DUO_LED = 25;
-    
+
     const stdout = std.io.getStdOut().writer();
     try stdout.print("hello\n", .{});
 
-    _= wx.wiringXSetup(&duo,null);
-    _= wx.pinMode(DUO_LED,wx.PINMODE_OUTPUT);
+    _ = wx.wiringXSetup(&board, null);
+    _ = wx.pinMode(DUO_LED, wx.PINMODE_OUTPUT);
 
-    while(true){
-        try stdout.print("Duo Led GPIO {wiringX} %d: High\n",.{DUO_LED});
-        _= wx.digitalWrite(DUO_LED, wx.HIGH);
+    while (true) {
+        try stdout.print("Duo Led GPIO  {}: High\n", .{DUO_LED});
+        _ = wx.digitalWrite(DUO_LED, wx.HIGH);
         std.time.sleep(1000000000);
-        try stdout.print("Duo Led GPIO {wiringX} %d: Low\n",.{DUO_LED});
-        _= wx.digitalWrite(DUO_LED, wx.LOW);
+        try stdout.print("Duo Led GPIO {}: Low\n", .{DUO_LED});
+        _ = wx.digitalWrite(DUO_LED, wx.LOW);
         std.time.sleep(1000000000);
     }
 }
